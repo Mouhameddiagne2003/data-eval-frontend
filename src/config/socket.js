129d3577694c71dd289a5d014192ee69f6711a33
Mouhameddiagne2003 <mouhameddiagne029@gmail.com>
2025-03-24 20:04:03 +0000

    fix: url bugs

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL); // Remplace avec l'URL de ton serveur
+const socket = io(import.meta.env.VITE_API_URL,{
+    transports: ["websocket", "polling"], // Active WebSocket et fallback en polling
+}); // Remplace avec l'URL de ton serveur
 
 export default socket;
