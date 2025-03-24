51698bfcda5dbab3dcf50ad5415694d7ae8c5652
Mouhameddiagne2003 <mouhameddiagne029@gmail.com>
2025-03-24 14:05:53 +0000

    refractor for deployment: add env variable for backend url

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Remplace avec l'URL de ton serveur
+const socket = io(import.meta.env.VITE_API_URL); // Remplace avec l'URL de ton serveur
 
 export default socket;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "http://localhost:5000/api"; // Remplace par ton URL API
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
